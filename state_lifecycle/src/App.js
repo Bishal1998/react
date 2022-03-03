76e2e1c25c75e4d01fcf7810477fbedd90aba936
“Bishal1998@gmail.com” <Bishalpaudel1998@gmail.com>
2022-03-03 21:50:19 +0545

    getDerivedStateFromProps

--- state_lifecycle/src/App.js
+++ state_lifecycle/src/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Student from './Student';
 
 export default class App extends Component {
 
   constructor(props){
     super(props);
     console.log("APP - Constructor Called")
+
+    this.state = {
+      roll : "101"
+    };
   }
+
+  static getDerivedStateFromProps(props, state){
+    console.log("APP - Get Derived State From Props");
+    console.log(props, state);
+    return null;
+  }
+
   render() {
     return (
       <div>
         <h1>Hello {this.props.name}</h1>
+        <h1>{this.state.roll}</h1>
+        <Student name = "Rahul" />
       </div>
     )
   }
